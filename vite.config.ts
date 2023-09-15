70ef2f11053c481a08fcf1a32e5b3f5970243e84
jundran <jundran@example.com>
2023-09-15 14:18:29 -0600

    Add base to Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
 	plugins: [react()],
+	base: '/odinbook-ts/',
 	resolve: {
 		alias: {
 			'@components': '/src/components',
